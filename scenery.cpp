624ba19eb1e60ab7e00759d5ff29ae9dc2cf10de
shristibansal <shristibansal6@gmail.com>
2022-01-31 01:05:59 +0530

    scenery

---
+++ scenery.cpp
@@ -0,0 +1,70 @@
+#include<graphics.h>
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+	int gd = DETECT, gm;
+	int a[10] = {300,200,450,200,510,300,360,300,300,200};
+	int a1[8] = {250,300,250,400,365,400,365,300};
+	int a2[6] = {365,400,500,370,500,300};
+	initgraph(&gd, &gm, "C:\\turboc3\\bgi");
+	
+	setcolor(6);
+	drawpoly(5,a);
+	drawpoly(4,a1);
+	drawpoly(3,a2);
+	
+	ellipse(80,220,162,270,45,25);
+	rectangle(80,220,150,350);
+	
+	line(300,200,247,305);
+	line(302,202,250,303);
+	
+	// window
+	circle(300,270,20);
+	
+	// door
+	rectangle(285,335,330,400);
+	
+	//land
+	line(0,350,250,350);
+	line(500,350,700,350);
+	
+	//sun
+	circle(360,80,30);
+	line(335,47,343,56);
+	line(377,56,392,38);
+	line(340,104,330,114);
+	line(370,107,381,122);
+	
+//	line(335,47,340,57);
+//	line(370,60,396,40);
+//	line(340,104,330,114);
+//	line(387,105,398,114);
+	
+	setfillstyle(1,1);
+	floodfill(300,201,6);
+	setfillstyle(9,1);
+	floodfill(305,201,6);
+	setfillstyle(1,7);
+	floodfill(360,350,6);
+	setfillstyle(1,7);
+	floodfill(370,350,6);
+	setfillstyle(9,1);
+	floodfill(300,340,6);
+	setfillstyle(9,1);
+	floodfill(300,270,6);
+	setfillstyle(1,10);
+	floodfill(1,380,6);
+	setfillstyle(1,LIGHTBLUE);
+	floodfill(10,3,6);
+	setfillstyle(1,14);
+	floodfill(360,80,6);
+	setfillstyle(1,6);
+	floodfill(90,250,6);
+	
+
+	
+	getch();
+	closegraph();
+}
