b30c0a6ab1ddeb2b5a225219d11e437344655a31
shristibansal <shristibansal6@gmail.com>
2022-01-31 20:00:21 +0530

    scenery

--- scenery.cpp
+++ scenery.cpp
@@ -3,19 +3,29 @@
 #include<stdio.h>
 int main()
 {
-	int gd = DETECT, gm;
+	int gd = DETECT, gm; 
 	int a[10] = {300,200,450,200,510,300,360,300,300,200};
 	int a1[8] = {250,300,250,400,365,400,365,300};
 	int a2[6] = {365,400,500,370,500,300};
 	initgraph(&gd, &gm, "C:\\turboc3\\bgi");
-	
+
 	setcolor(6);
 	drawpoly(5,a);
 	drawpoly(4,a1);
 	drawpoly(3,a2);
 	
-	ellipse(80,220,162,270,45,25);
-	rectangle(80,220,150,350);
+	ellipse(80,220,162,270,45,35);
+	ellipse(72,192,143,244,55,35);
+	ellipse(82,178,104,198,55,40);
+	
+//	ellipse(86,170,65,134,55,45);
+	ellipse(106,169,32,139,55,45);
+	
+	//ellipse(137,168,49,110,55,38);
+	ellipse(152,174,337,96,45,35);	
+	ellipse(168,201,287,57,45,35);
+	ellipse(150,220,270,9,45,35);
+	rectangle(80,242,150,350);
 	
 	line(300,200,247,305);
 	line(302,202,250,303);
@@ -62,6 +72,8 @@ int main()
 	floodfill(360,80,6);
 	setfillstyle(1,6);
 	floodfill(90,250,6);
+	setfillstyle(1,GREEN);
+	floodfill(80,200,6);
 	
 
 	
